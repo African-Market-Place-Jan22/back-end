113a82c05f4b5289f5ed353c5179a03312cabcdf
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2022-02-01 15:09:57 -0700

    errors

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -10,7 +10,7 @@ const {
 
 router.post(
     "/register",
-    checkUsernameTaken,
+   checkUsernameTaken,
     (req, res, next) => {
      const { username, password } = req.body;
      const hash = bcrypt.hashSync(password, 8);
@@ -22,19 +22,33 @@ router.post(
    
     },
     );
+
+    router.post('/login',checkUsernameExists,(req, res) => {
+  
+        let { username, password } = req.body;
+      
+        
+        User.findBy({ username })
+          .then(([user]) => {
+            
+      
+            if (user && bcrypt.compareSync(password, user.password)) {
+              
+              const token = tokenBuilder(user);
+      
+              
+              res.status(200).json({ message: "Welcome!", token, id: user.id });
+            } else {
+              res.status(401).json({ message: "Invalid credentials" });
+            }
+          })
+          .catch(error => {
+            console.log(error);
+            res.status(500).json({ errorMessage: error.message });
+          });
+      });
     
-    router.post(
-     "/login",
-     checkUsernameExists,
-     (req, res) => {
-      if (bcrypt.compareSync(req.body.password, req.user.password)) {
-        const token = tokenBuilder(req.user)
-        const { user_id } = req.user;
-        res.status(200).json({ message: `Welcome ${req.user.username}!`, user_id, token })
-    } else {
-        res.status(401).json({ message: "Invalid credentials" })
-    }
-})
+
    
     module.exports = router;
    
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,27 @@
-/*const db = require('../data/db-configrs')
+const db = require('../data/db-configrs')
 
 function find() {
     return db(`users`)
     .select('user_id', 'username')
 }
-function findBy(filter){
-    return db('users')
-    .select('user_id','username','password')
-    .where(filter)
+
+function findBy(filter) {
+  return db("users").where(filter);
 }
+
 function findById(id) {
     return db('users')
-    .select('user_id', 'username')
     .where({ id })
     .first();
   }
 
-async function add(user) {
-    const [id] = await db('users').insert(user)
+  async function add(user) {
+    const [id] = await db("users").insert(user, "id");
     return findById(id)
   }
-
   module.exports = {
     find,
     findBy,
     findById,
     add
-  }*/
+  }
