56d62b1330dd2795f6300b91740a27f603279b87
Peter Conley <peterdavidconley@gmail.com>
2022-02-02 09:39:31 -0500

    wrote validateItemId function

--- api/items/items-middleware.js
+++ api/items/items-middleware.js
@@ -1,7 +1,26 @@
 const Item = require('./items-model');
 
-function checkItemId (req, res, next) {
-    console.log(req)
+function logger(req,res, next){
+    console.log(`logger middleware`)
+    next()
+}
+
+async function validateItemId (req, res, next) {
+    try{
+        const item = await Item.getById(req.params.id)
+        if (!item) {
+            res.status(404).json({
+                message: 'no such item'
+            })
+        } else {
+            req.item = item
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'Problem fetching item.'
+        })
+    }
 }
 
 function validateItem (req, res, next) {
